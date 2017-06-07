c7b10d18b1d5c3a52335e93de666e2358dde96f5
inthej <inthej12@naver.com>
2017-06-07 15:36:39 +0900

    CartConfirmPanel -> OrderConfirmPanel Class 재정의

--- src/dev/mcdonaldkiosk/page/menu/order/OrderConfirmPanel.java
+++ src/dev/mcdonaldkiosk/page/menu/order/OrderConfirmPanel.java
@@ -8,15 +8,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import dev.mcdonaldkiosk.lang.LangCheck;
-import dev.mcdonaldkiosk.main.MainFrame;
-import dev.mcdonaldkiosk.page.confirm.ConfirmPage;
-import dev.mcdonaldkiosk.page.menu.OrderDataList;
 import dev.mcdonaldkiosk.util.KioskAudioPlayer;
 
 /**
  * 장바구니 패널의 선택메뉴정보 확인 패널
  */
-class CartConfirmPanel extends JPanel {
+class OrderConfirmPanel extends JPanel {
 
 	private final JLabel ORDER_TEXT_LABEL = new JLabel();
 	private final JLabel ORDER_DATA_LABEL = new JLabel();
@@ -26,17 +23,21 @@ class CartConfirmPanel extends JPanel {
 	private final Color BACKGROUND_COLOR = Color.WHITE;
 	private final Color CANCLE_BTN_COLOR = Color.GRAY;
 	private final Color PAYMENT_BTN_COLR = Color.ORANGE;
+	
+	private int totalQuantity;
+	private int totalAmount;
+	private int totalKCal;
 
-	private final OrderDataList ORDER_DATA_LIST;
-
-	CartConfirmPanel(OrderDataList orderDataList) {
-		this.ORDER_DATA_LIST = orderDataList;
-
+	OrderConfirmPanel(int totalQuantity, int totalAmount, int totalKCal) {
+		this.totalQuantity = totalQuantity;
+		this.totalAmount = totalAmount;
+		this.totalKCal = totalKCal;
+		
 		initOrderConfirmPanel();
-		initOrderTextLabel();
-		initOrderDataLabel();
-		initCancleButton();
-		initPaymentButton();
+
+		initTextLabel();
+		initDataLabel();
+		initButton();
 
 		setListener();
 	}
@@ -44,57 +45,56 @@ class CartConfirmPanel extends JPanel {
 	private void initOrderConfirmPanel() {
 		this.setLayout(new GridLayout(0, 4));
 		this.setBackground(BACKGROUND_COLOR);
+		this.add(ORDER_TEXT_LABEL);
+		this.add(ORDER_DATA_LABEL);
+		this.add(CANCLE_BUTTON);
+		this.add(PAYMENT_BUTTON);
 	}
 
-	private void initOrderTextLabel() {
+	private void initTextLabel() {
 		ORDER_TEXT_LABEL.setText(LangCheck.isKorean() ? "<html>주문수량<br>주문금액<br>총 칼로리</html>"
 				: "<html>Order quantity<br>Order amount<br>Total calories</html>");
 		ORDER_TEXT_LABEL.setHorizontalAlignment(JLabel.CENTER);
-
-		this.add(ORDER_TEXT_LABEL);
 	}
-
-	private void initOrderDataLabel() {
-		ORDER_DATA_LABEL.setText("<html>" + ORDER_DATA_LIST.getOrderQuantity() + "<br>"
-				+ ORDER_DATA_LIST.getOrderAmount() + "<br>" + ORDER_DATA_LIST.getOrderKCal() + "</html>");
+	
+	private void initDataLabel() {
+		ORDER_DATA_LABEL.setText("<html>" + totalQuantity + "<br>" + totalAmount + "<br>" + totalKCal + "</html>");
 		ORDER_DATA_LABEL.setHorizontalAlignment(JLabel.CENTER);
-
-		this.add(ORDER_DATA_LABEL);
 	}
-
-	private void initCancleButton() {
+	
+	private void initButton() {
 		CANCLE_BUTTON.setText(LangCheck.isKorean() ? "주문취소" : "CANCEL ORDER");
 		CANCLE_BUTTON.setBackground(CANCLE_BTN_COLOR);
-
-		this.add(CANCLE_BUTTON);
-	}
-
-	private void initPaymentButton() {
+		
 		PAYMENT_BUTTON.setText(LangCheck.isKorean() ? "주문진행" : "COMPLETE ORDER");
 		PAYMENT_BUTTON.setBackground(PAYMENT_BTN_COLR);
-
-		this.add(PAYMENT_BUTTON);
 	}
 
 	private void setListener() {
 		CANCLE_BUTTON.addActionListener((e) -> {
-			MyOrderPanel.clearOrderData();
+			// TODO : 주문비우기
+			// MyOrderPanel.clearOrderData();
 		});
 
 		PAYMENT_BUTTON.addActionListener((e) -> {
-			if (ORDER_DATA_LIST.getOrderQuantity() == 0) {
+			if (totalQuantity == 0) {
 				KioskAudioPlayer kioskAudioPlayer = KioskAudioPlayer
 						.createKioskAudioPlayer(LangCheck.isKorean() ? "sound/order.wav" : "sound/order_eng.wav");
 				kioskAudioPlayer.play();
 			} else {
-				MainFrame.attachPanel(new ConfirmPage(ORDER_DATA_LIST));
+				// TODO : 다음페이지 데이터 넘기기.
+				// MainFrame.attachPanel(new ConfirmPage(ORDER_DATA_LIST));
 			}
 		});
 	}
+	
+	void setOrderData(int totalQuantity, int totalAmount, int totalKCal) {
+		this.totalQuantity = totalQuantity;
+		this.totalAmount = totalAmount;
+		this.totalKCal = totalKCal;
+	}
 
-	// 데이터라벨 리셋
-	void refleshData() {
-		ORDER_DATA_LABEL.setText("<html>" + ORDER_DATA_LIST.getOrderQuantity() + "<br>"
-				+ ORDER_DATA_LIST.getOrderAmount() + "<br>" + ORDER_DATA_LIST.getOrderKCal() + "</html>");
+	void refresh() {
+		ORDER_DATA_LABEL.setText("<html>" + totalQuantity + "<br>" + totalAmount + "<br>" + totalKCal + "</html>");
 	}
 }
