748d946306b313435d42eb4e0df2729db8344c5b
Jaehyeon Kim <inthej12@naver.com>
2017-06-22 22:55:31 +0900

    ConfirmButton 정리

--- src/dev/mcdonaldkiosk/page/confirm/ConfirmButton.java
+++ src/dev/mcdonaldkiosk/page/confirm/ConfirmButton.java
@@ -6,24 +6,40 @@ import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 
 /**
- * Created by kimjaehyeon
- * Class Role : ConfirmPage에서 사용되는 버튼의 rollover 효과를 담당한다.
+ * Class Role : ConfirmPage에서 사용되는 디자인이 적용된 버튼이다.
+ * 버튼의 배경컬러와 Rollover 효과가 적용되어 있다.
+ *
+ * @authro Jaehyeon Kim
+ * @see ConfirmPage
  */
 class ConfirmButton extends JButton {
 
-  ConfirmButton(String text) {
-    super(text);
-    this.setBackground(Color.WHITE);
+  private final Color backgroundColor = Color.WHITE;
 
+  private final Color baseColor = Color.WHITE;
+  private final Color hoverColor = Color.ORANGE;
+
+  ConfirmButton(final String text) {
+    super(text);
+
+    initBackground();
+    setListener();
+  }
+
+  private void initBackground() {
+    this.setBackground(backgroundColor);
+  }
+
+  private void setListener() {
     this.addMouseListener(new MouseAdapter() {
       @Override
-      public void mouseEntered(MouseEvent e) {
-        ConfirmButton.this.setBackground(Color.ORANGE);
+      public void mouseEntered(final MouseEvent e) {
+        ConfirmButton.this.setBackground(hoverColor);
       }
 
       @Override
-      public void mouseExited(MouseEvent e) {
-        ConfirmButton.this.setBackground(Color.WHITE);
+      public void mouseExited(final MouseEvent e) {
+        ConfirmButton.this.setBackground(baseColor);
       }
     });
   }
